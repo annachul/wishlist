2ee0c0ff1ccef47c28669a85fe457724ef7420b1
Анна <anna@MacBook-Air-Anna.local>
2021-11-03 18:53:01 +0400

    init

--- gifts/views.py
+++ gifts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Gifts
+from .forms import GiftConformation
 
 # Create your views here.
 
@@ -12,14 +13,24 @@ def index(request):
 
 def giftdetail(request, id):
     try:
-        giftselect = Gifts.objects.get(id=id)
-        gifts = Gifts.objects.all()
-        return render(request,'gifts/giftdetail.html',  {
-            'gift_found': True,
-            'gifttitle': giftselect.title,
-            'gifdescription': giftselect.description,
-            'gifturl':giftselect.url,
-            'giftimage':giftselect.image
-    })
+        if request.method == 'GET':
+            giftselect = Gifts.objects.get(id=id)
+            gifts = Gifts.objects.all()
+            gift_conformation = GiftConformation()
+            return render(request,'gifts/giftdetail.html',  {
+                'gift_found': True,
+                'gifttitle': giftselect.title,
+                'gifdescription': giftselect.description,
+                'gifturl':giftselect.url,
+                'giftimage':giftselect.image,
+                'form':gift_conformation,
+                'taken':giftselect.taken
+             })
+        else:
+            giftselect = Gifts.objects.get(id=id)
+            gifts = Gifts.objects.all()
+            giftselect.taken=True
+            giftselect.save()
+            return redirect('/gifts/')
     except:
         return render(request,'gifts/giftdetail.html', {'gift_found': False})
